6b2be911a8c1223c126b54c19cccd2390cc44cea
Steven liu <steven.liu@macquarie.com>
2016-11-07 18:28:40 +1100

    Getting rid of jdee as well

--- init.el
+++ init.el
@@ -392,15 +392,15 @@
   :ensure t
   :demand)
 
-(use-package jdee
-  :ensure t
-  :config
-  (if (eq nil (getenv "JDEE_SERVER_DIR"))
-      (setenv "JDEE_SERVER_DIR" "~/.emacs.d/jdee/jdee-server/jdee-server-master"))
-  (setq jdee-server-dir (getenv "JDEE_SERVER_DIR"))
-  (setq jdee-complete-function 'jdee-complete-minibuf)
-  (define-key jdee-mode-map (kbd "M-'") 'jdee-complete)
-  :demand)
+;;(use-package jdee
+;;  :ensure t
+;;  :config
+;;  (if (eq nil (getenv "JDEE_SERVER_DIR"))
+;;      (setenv "JDEE_SERVER_DIR" "~/.emacs.d/jdee/jdee-server/jdee-server-master"))
+;;  (setq jdee-server-dir (getenv "JDEE_SERVER_DIR"))
+;;  (setq jdee-complete-function 'jdee-complete-minibuf)
+;;  (define-key jdee-mode-map (kbd "M-'") 'jdee-complete)
+;;  :demand)
 
 ;;(use-package eclim
 ;;  :ensure t
