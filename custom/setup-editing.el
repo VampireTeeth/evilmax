0e0e75053807e80016275c0acb02d59fd58ffe5a
Steven liu <steven.liu@macquarie.com>
2017-05-10 18:11:47 +1000

    Added rainbow-delimiters mode

--- custom/setup-editing.el
+++ custom/setup-editing.el
@@ -94,10 +94,29 @@
   :init
   (setq iedit-toggle-key-default nil))
 
-(use-package highlight-parentheses
+;;(use-package highlight-parentheses
+;;  :ensure t
+;;  :init
+;;  (global-highlight-parentheses-mode t))
+
+(use-package rainbow-delimiters
   :ensure t
-  :init
-  (global-highlight-parentheses-mode t))
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(rainbow-delimiters-depth-1-face ((t (:foreground "medium slate blue"))))
+   '(rainbow-delimiters-depth-2-face ((t (:foreground "deep sky blue"))))
+   '(rainbow-delimiters-depth-3-face ((t (:foreground "pale green"))))
+   '(rainbow-delimiters-depth-4-face ((t (:foreground "gold"))))
+   '(rainbow-delimiters-depth-5-face ((t (:foreground "indian red"))))
+   '(rainbow-delimiters-depth-6-face ((t (:foreground "orange red"))))
+   '(rainbow-delimiters-depth-7-face ((t (:foreground "plum"))))
+   '(rainbow-delimiters-depth-8-face ((t (:foreground "snow1"))))
+   '(rainbow-delimiters-depth-9-face ((t (:foreground "light slate gray"))))))
 
 ;; Customized functions
 (defun prelude-move-beginning-of-line (arg)
--- init.el
+++ init.el
@@ -48,27 +48,3 @@
 ;;(setq company-backends (delete 'company-semantic company-backends))
 ;;(define-key c-mode-map  [(tab)] 'company-complete)
 ;;(define-key c++-mode-map  [(tab)] 'company-complete)
-
-
-
-;; function-args
-;; (require 'function-args)
-;; (fa-config-default)
-;; (define-key c-mode-map  [(tab)] 'company-complete)
-;; (define-key c++-mode-map  [(tab)] 'company-complete)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu)))
- ;;'(safe-local-variable-values (quote ((cmake-ide-build-dir pwd))))
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
