0c8ca4d22cad80952919e3df2b90f3c42884890f
Steven liu <steven.liu@macquarie.com>
2017-05-31 09:58:06 +1000

    Adding clj-refactor

--- custom/setup-clojure-ide.el
+++ custom/setup-clojure-ide.el
@@ -1,6 +1,18 @@
 (use-package cider
   :ensure t
   :config
-  (message "Cider is loaded"))
+  (message "Cider is loaded")
+  (add-hook 'cider-repl-mode-hook #'company-mode)
+  (add-hook 'cider-mode-hook #'company-mode)
+  ;;(remove-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
+  ;;(remove-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
+  )
+
+(use-package clj-refactor
+  :ensure t
+  :config
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1)
+  (cljr-add-keybindings-with-prefix "C-c C-f"))
 
 (provide 'setup-clojure-ide)
