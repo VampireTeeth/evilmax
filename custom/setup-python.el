2398f0ebf4097b76f6074578c166000e50ac2c36
VampireTeeth <vampireteeth001@gmail.com>
2018-10-13 15:51:08 +1100

    Tweaks on python setup

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -1,14 +1,11 @@
 (use-package python-mode
   :ensure t
   :config
-  (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-  )
+  (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode)))
 
 (use-package elpy
   :ensure t
   :config
-  (add-to-list 'auto-mode-alist '("\\.py\\'" . elpy-mode))
-  (add-hook 'elpy-mode 'python-mode)
-  (add-hook 'elpy-mode 'elpy-enable))
+  (elpy-enable))
 
 (provide 'setup-python)
