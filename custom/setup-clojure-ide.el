8a8d6e9b14c1cdf23899895558d5a15ee4020757
VampireTeeth <vampireteeth001@gmail.com>
2019-05-22 06:54:13 +1000

    Minor change in clojure setup

--- custom/setup-clojure-ide.el
+++ custom/setup-clojure-ide.el
@@ -6,14 +6,14 @@
   (add-hook 'cider-mode-hook #'company-mode)
   ;;(remove-hook 'cider-repl-mode-hook #'cider-company-enable-fuzzy-completion)
   ;;(remove-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion)
-  )
+  (use-package clj-refactor
+    :ensure t
+    :config
+    (add-hook 'clojure-mode-hook (lambda ()
+                                   (clj-refactor-mode 1)))
+    (yas-minor-mode 1)
+    (cljr-add-keybindings-with-prefix "C-c C-r C-f")))
 
-(use-package clj-refactor
-  :ensure t
-  :config
-  (add-hook 'clojure-mode-hook (lambda ()
-                                (clj-refactor-mode 1)))
-  (yas-minor-mode 1)
-  (cljr-add-keybindings-with-prefix "C-c C-r C-f"))
+
 
 (provide 'setup-clojure-ide)
