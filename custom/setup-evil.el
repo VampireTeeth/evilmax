c51fbb442340133db2ebef068ebe9787eb468767
Steven Liu <sliu2@atlassian.com>
2019-07-08 11:52:41 +1000

    Avy keybinding in my-leader-map

--- custom/setup-evil.el
+++ custom/setup-evil.el
@@ -1,15 +1,17 @@
 (use-package evil
   :ensure t
   :config
-  (defvar my-leader-map (make-sparse-keymap)"Keymap for \"leader key\" shortcuts.")
-  (define-key evil-normal-state-map "," my-leader-map)
-  ;; binding ",b"
+  (defvar my-leader-map (make-sparse-keymap) "Keymap for \"leader key\" shortcuts.")
+  (define-key evil-normal-state-map (kbd "<SPC>") my-leader-map)
+  ;; binding "<SPC>b"
   (define-key my-leader-map "b" 'list-buffers)
+  (define-key my-leader-map "jj" 'evil-avy-goto-char)
+  (define-key my-leader-map "jc" 'evil-avy-goto-char-2)
+  (define-key my-leader-map "jw" 'evil-avy-goto-word-1)
   (define-key evil-normal-state-map (kbd "C-n") nil)
   (define-key evil-normal-state-map (kbd "C-p") nil)
   (evil-mode 1)
   (setq-default evil-shift-width 2))
-
 (use-package evil-surround
   :ensure t
   :config
